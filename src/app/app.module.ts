640c4bfa85dfc715755c8f9ef79ca91351f2bd8a
Josh Nguyen <khoixkt@gmail.com>
2018-11-15 08:47:35 -0600

    creating ng7 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { MapComponent } from './map/map.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NewsComponent } from './news/news.component';
+import { PinsComponent } from './pins/pins.component';
+import { InfoComponent } from './info/info.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    SignupComponent,
+    LoginComponent,
+    MapComponent,
+    ProfileComponent,
+    NewsComponent,
+    PinsComponent,
+    InfoComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
