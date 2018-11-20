c220679ed7b706d792c1560075c257a887df98b4
Josh Nguyen <khoixkt@gmail.com>
2018-11-20 17:35:20 -0600

    added css with working side menu

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from './ui/ui.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  showMenu = false;
+  darkModeActive: boolean;
 
-  constructor() { }
+  constructor(public ui: UiService) {
+    
+  }
 
   ngOnInit() {
+    this.ui.darkModeState.subscribe((value) => {
+      this.darkModeActive = value;
+    });
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  modeToggleSwitch() {
+    this.ui.darkModeState.next(!this.darkModeActive);
   }
 
 }
