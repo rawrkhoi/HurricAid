c4b53e05c23908006d68fe9421c9e8e9f5dadae1
Deja Trudeaux <dejadej@gmail.com>
2018-11-20 13:46:00 -0600

    adding files to be same as upstream

--- server/index.js
+++ server/index.js
@@ -13,27 +13,6 @@ const app = express();
 app.use(express.static(`${__dirname}/../dist/emergency`));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-<<<<<<< HEAD
-// app.get('/', (req, res) => {
-//  res.send('done');
-// })
-
-app.post('/test', (req, res) => {
-
-})
-
-app.get('/test', (req, res) => {
-  client.messages.create({
-    body: 'Greetings! The current time is: XXXXXX GFYBF2Y40TH1T7Y',
-    to: '+12092104311',  // Text this number
-    from: '+15043020292' // From a valid Twilio number
-  })
-  .then((message) => console.log(message.sid))
-  .done();
-}); 
-
-=======
->>>>>>> 562137e805269330f5f9f6956f136f65628a7133
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
   twiml.message('Hi! It looks like your phone number was born ' + req.body.FromCountry );
