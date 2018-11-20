29b87b4169e89f9eb3f8f53c33e10e2eacf78008
Deja Trudeaux <dejadej@gmail.com>
2018-11-20 12:15:39 -0600

    trying to merge

--- database/index.js
+++ database/index.js
@@ -13,23 +13,4 @@ sequelize.authenticate()
   .catch((err) => {
     console.log('Error connecting to database', err);
   });
-<<<<<<< HEAD
-
-const User = sequelize.define('user', {
-  firstName: {
-    type: Sequelize.STRING
-  },
-  lastName: {
-    type: Sequelize.STRING
-  }
-  });
-
-sequelize.sync({ force: false })
-  .then(() => {
-    console.log('It worked!');
-  }, (err) => {
-    console.log('An error occurred while creating the table:', err);
-});
-=======
   
->>>>>>> 3eff553f124edf01181429a468799c7f8d48c5f5
