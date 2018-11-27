39c0b81878a1f444de4930ab578a78c7bc031e65
Josh Nguyen <khoixkt@gmail.com>
2018-11-27 14:58:03 -0600

    Updated signup/login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required]),
+  });
+
+  constructor(private http: HttpClient, private router: Router, private userService: UserService) { }
 
   ngOnInit() {
   }
-
+  moveToMap() {
+    this.router.navigate(['/map']);
+  }
+  login() {
+    if (!this.loginForm.valid) {
+      console.log('Invalid'); return;
+    }
+    this.userService.login(JSON.stringify(this.loginForm.value))
+      .subscribe(data => {
+        console.log(data);
+        this.router.navigate(['/map']);
+      },
+        error => { console.log(error); }
+      )
+    // console.log(JSON.stringify(this.loginForm.value));
+  }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,31 +10,38 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  model: any = {};
-  
 
-  constructor(private http: HttpClient) { }
+  model: any = {};
+  signUpForm: FormGroup = new FormGroup({
+    firstName: new FormControl(null, [Validators.required]),
+    lastName: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required]),
+    cpassword: new FormControl(null, [Validators.required]),
+    phone: new FormControl(null, [Validators.required]),
+    address: new FormControl(null, [Validators.required]),
+  });
+
+  constructor(private http: HttpClient, private router: Router, private userService: UserService) { }
 
   ngOnInit() {
   }
-
-  signupUser() {
-    let newObj = {
-      first_name: this.model.firstName,
-      last_name: this.model.lastName,
-      email: this.model.email,
-      password: this.model.createPassword,
-      photo_url: this.model.profImg,
-      emergency_contact: this.model.emergencyContact,
-      phone_id: this.model.addPhone,
-      location_id: {
-        city: this.model.city,
-        state: this.model.state,
-        zip: this.model.zip
-      }
-    }
-    console.log(newObj);
-    this.http.post('/signup', newObj);
+  moveToMap() {
+    this.router.navigate(['/map']);
   }
-
+  moveToLogin() {
+    this.router.navigate(['/login']);
+  }
+  signUp() {
+    console.log('clicked');
+    if (!this.signUpForm.valid || this.signUpForm.controls.password.value !== this.signUpForm.controls.cpassword.value) {
+      console.log('Invalid Form'); return;
+    }
+    this.userService.signUp(JSON.stringify(this.signUpForm.value))
+      .subscribe(data => {
+        console.log(data);
+        this.router.navigate(['/login']);
+      })
+    // console.log(JSON.stringify(this.signUpForm.value));
+  }
 }
