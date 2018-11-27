ae50c28dfcfdd09879e43071edc609877336e45e
Josh Nguyen <khoixkt@gmail.com>
2018-11-27 15:01:03 -0600

    created user service

---
+++ src/app/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  signUp(body: any) {
+    return this.http.post('http://127.0.0.1:3000/users/signup', body, {
+      observe: 'body',
+      headers: new HttpHeaders().append('Content-Type', 'application/json')
+    });
+  }
+  login(body: any) {
+    return this.http.post('http://127.0.0.1:3000/users/login', body, {
+      observe: 'body',
+      withCredentials: true,
+      headers: new HttpHeaders().append('Content-Type', 'application/json')
+    });
+  }
+}
