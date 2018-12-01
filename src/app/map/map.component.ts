d918953ef33368ab40fe03be17f933bf228ecc3e
Josh Nguyen <khoixkt@gmail.com>
2018-12-01 16:24:21 -0600

    Only see help pins on login

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -4,6 +4,8 @@ import { HttpClient } from '@angular/common/http';
 import { HelppinComponent } from '../helppin/helppin.component';
 import { HavepinComponent } from '../havepin/havepin.component';
 import { MatDialog } from '@angular/material';
+import { Observable } from 'rxjs';
+import { AuthService } from '../service/auth.service'
 import * as moment from 'moment';
 
 @Component({
@@ -12,8 +14,9 @@ import * as moment from 'moment';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  isLoggedIn$: Observable<boolean>;
 
-  zoom: number = 8;
+  zoom: number = 12;
   lat: any;
   lng: any;
   markers: any = [];
@@ -21,19 +24,18 @@ export class MapComponent implements OnInit {
   haveUrl = 'https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|4286f4';
   helpUrl = 'https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|FF0000';
 
-  constructor(private map: MapsService, private http: HttpClient, public dialog: MatDialog) {
+  constructor(
+    private map: MapsService,
+    private http: HttpClient,
+    public dialog: MatDialog,
+    private authService: AuthService) {
   }
 
   ngOnInit() {
+    this.isLoggedIn$ = this.authService.isLoggedIn;
+
     // location by browser or by ip if error or navigator unavailable
-    if (!navigator.geolocation) {
-      this.map.getLocation().subscribe(data => {
-        this.lat = data.latitude;
-        this.lng = data.longitude;
-      });
-    }
     const success = (position) => {
-      console.log(this);
       this.lat = position.coords.latitude;
       this.lng = position.coords.longitude;
     }
@@ -47,6 +49,12 @@ export class MapComponent implements OnInit {
 
     navigator.geolocation.getCurrentPosition(success, error);
 
+    if (!navigator.geolocation || (!this.lat && !this.lng)) {
+      this.map.getLocation().subscribe(data => {
+        this.lat = data.latitude;
+        this.lng = data.longitude;
+      });
+    }
     // query all the pins from db and push to markers
     this.http.get('/getPins').subscribe((pins: any) => {
       for (let i = 0; i < pins.length; i ++) {
