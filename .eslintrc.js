1ed01a7b2050cda1ae2b519e114509dc620a4372
Josh Nguyen <khoixkt@gmail.com>
2018-12-03 17:08:34 -0600

    added linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,12 @@
 module.exports = {
-  "extends": ["airbnb", "tslint-config-airbnb", "tslint-eslint-rules"],
+  "extends": "airbnb",
   "rules": {
     // enable additional rules
     "indent": ["error", 2],
     "linebreak-style": ["error", "unix"],
     "semi": ["error", "always"],
-
     // override default options for rules from base configurations
     "no-cond-assign": ["error", "always"],
-    "no-constant-condition": true,
     // disable rules from base configurations
     "no-console": "off",
   }
