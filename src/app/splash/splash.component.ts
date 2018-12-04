2a59176d7a202dfe076bc4f128e0d6c5bbb76581
Ethan Hess <erhess0011@gmail.com>
2018-12-03 19:35:53 -0600

    added ticker to splash page

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -4,6 +4,9 @@ import { SignupComponent } from '../signup/signup.component';
 import { LoginComponent } from '../login/login.component';
 import { InfoComponent } from '../info/info.component';
 import { Router } from '@angular/router';
+import { MapsService } from '../service/maps.service';
+import { HttpClient } from '@angular/common/http';
+import { keys } from 'config';
 
 export interface DialogData {
 
@@ -16,10 +19,29 @@ export interface DialogData {
 })
 export class SplashComponent implements OnInit {
 
+  lat: string = '';
+  lng: string = '';
   imgSrc: string = '../../hurricane.png'; 
-  constructor(public dialog: MatDialog, private router: Router) { }
+  article: any;
+  newsDisplay: boolean = false;
+
+  constructor(public dialog: MatDialog, private router: Router, private map: MapsService, private http: HttpClient) { }
 
   ngOnInit() {
+  this.map.getLocation().subscribe(data => {
+  // console.log(data);
+  this.lat = data.latitude;
+  this.lng = data.longitude;
+  this.http.get((`https://api.predicthq.com/v1/events/?category=severe-weather,disasters,terror&within=10km@${this.lat},${this.lng}`), 
+    { headers: { Authorization: `Bearer ${keys.predictHQ}` }})
+      .subscribe((response: any) => {
+        if (response['results'].length === 0){
+          this.newsDisplay = true;
+        } else {
+          this.article = response.results[0].title + ' ' + response.results[0].description;
+        }
+      })
+    })
   }
   moveToMap() {
     this.router.navigate(['/map']);
