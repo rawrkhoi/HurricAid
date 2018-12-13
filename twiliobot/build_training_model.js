77750d57fa6720e4ae51ce7270eb6093d64628a3
Deja Trudeaux <dejadej@gmail.com>
2018-12-12 20:37:28 -0600

    adding one have per sentence, removing correctly

--- twiliobot/build_training_model.js
+++
@@ -1,9 +0,0 @@
-const { config } = require('../config');
-const client = require('twilio')(config.accountSid, config.authToken);
-
-
-client.autopilot.assistants(config.assistant)
-  .modelBuilds
-  .create({ uniqueName: 'v0.1' })
-  .then(model_build => console.log(model_build.sid))
-  .done();
